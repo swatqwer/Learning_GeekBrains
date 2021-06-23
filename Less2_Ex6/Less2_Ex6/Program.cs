2613416eec23e82818503fbef4daf1af16cc0197
Anatoliy <swatqwer@gmail.com>
2021-06-23 23:47:09 +0700

    Урок 2, задание 6

---
+++ Less2_Ex6/Less2_Ex6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Less2_Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            table firstOfficeDay = (table)0b_0011111;
+            table secondOfficeDay = (table)0b_1111111;
+            table thirdOfficeDay = (table)0b_1100110;
+            Console.WriteLine($"Рабочий график первого офиса: {firstOfficeDay}");
+
+            Console.WriteLine($"\nРабочий график второго офиса: {secondOfficeDay}");
+
+            Console.WriteLine($"\nРабочий график третьего офиса: {thirdOfficeDay}");
+            Console.ReadKey();
+        }
+    }
+}
