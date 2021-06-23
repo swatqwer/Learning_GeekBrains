813b2bc37e24bbfab9a9cf7d9e8fea94d9e1e13d
Anatoliy <swatqwer@gmail.com>
2021-06-23 17:36:18 +0700

    Урок 2, задание 3

---
+++ Less2_Ex3/Less2_Ex3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Less2_Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число");
+            if (Int32.TryParse(Console.ReadLine(), out int number))
+            {
+                if (number / 2 != 0)
+                {
+                    Console.WriteLine("Это четное число");
+                }
+                else
+                {
+                    Console.WriteLine("Это нечетное число");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введённые данные не являются целым числом, либо содержит недопустимые символы");
+            }
+            Console.ReadKey();
+        }
+    }
+}
