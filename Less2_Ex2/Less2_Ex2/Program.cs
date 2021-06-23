1d42b426b173eb83e7e4443d1addaf0c45603592
Anatoliy <swatqwer@gmail.com>
2021-06-23 17:34:15 +0700

    Урок 2, задание 2

---
+++ Less2_Ex2/Less2_Ex2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Less2_Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер месяца (1-12)");
+            if (Int32.TryParse(Console.ReadLine(), out int month))
+            {
+                if (month <= 12 && month > 0)
+                {
+                    Console.WriteLine((Month)month);
+                }
+                else
+                {
+                    Console.WriteLine("Такого месяца не существует");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введён неверный символ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
