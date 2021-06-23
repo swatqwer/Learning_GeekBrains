0e32f5756923717056545cb65b05e22c8997781f
Anatoliy <swatqwer@gmail.com>
2021-06-23 17:30:22 +0700

    Урок 2, задание 1

---
+++ Less2_Ex1/Less2_Ex1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Less2_Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите максимальную температуру за сутки в °С");
+            var maxtempnum = Console.ReadLine();
+            if (!Int32.TryParse(maxtempnum, out int maxtempn))
+            {
+                Console.WriteLine("Некорректное число");
+                return;
+            }
+
+            Console.WriteLine("Введите минимальную температуру за сутки в °С");
+            var mintempnum = Console.ReadLine();
+            if (!Int32.TryParse(mintempnum, out int mintempn))
+            {
+                Console.WriteLine("Некорректное число");
+                return;
+            }
+            if (maxtempn < mintempn)
+            {
+                Console.WriteLine("Введённая минимальная температура превышает максимальную, проверьте введённые данные");
+            }
+            else
+            {
+                Console.Clear();
+                double remnant = (double)(maxtempn + mintempn) / 2;
+                Console.WriteLine($"Среднесуточная температура: {remnant} °С");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
