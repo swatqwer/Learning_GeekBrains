1c8ca21763942117921133bf0e821504ba69420a
Anatoliy <swatqwer@gmail.com>
2021-06-28 19:10:40 +0700

    Урок 3, задание 4

---
+++ Less3_Ex4/Less3_Ex4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Less3_Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] field = {
+                { "X", "O", "O", "O", "O", "O", "X", "X", "O", "O",},
+                { "X", "O", "O", "O", "X", "O", "O", "O", "O", "O",},
+                { "X", "O", "X", "O", "O", "O", "O", "X", "O", "O",},
+                { "X", "O", "X", "O", "O", "O", "O", "O", "O", "O",},
+                { "O", "O", "X", "O", "O", "X", "X", "O", "O", "O",},
+                { "O", "O", "O", "O", "O", "O", "O", "O", "O", "O",},
+                { "O", "O", "O", "O", "O", "O", "O", "X", "O", "O",},
+                { "O", "X", "O", "O", "O", "O", "O", "O", "O", "O",},
+                { "O", "X", "O", "O", "X", "X", "X", "O", "O", "O",},
+                { "O", "O", "O", "O", "O", "O", "O", "O", "O", "X",} };
+
+            Console.WriteLine("Поле боя:");
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    Console.Write($"{field[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
