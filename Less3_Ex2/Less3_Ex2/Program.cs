e2f28924e31d40c8690234ce2016e39a15e54324
Anatoliy <swatqwer@gmail.com>
2021-06-28 18:43:56 +0700

    Урок 3, задание 2

---
+++ Less3_Ex2/Less3_Ex2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Less3_Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] array2 = {{ "Иванов" }, {"8-800-555-3535"},
+                                { "Cерго" }, {"sergey18@email.ru"},
+                                { "Петров" }, {"8-800-00-0102"},
+                                { "Васечкин" }, {"vasek228@mail.ru"},
+                                { "Дмитриев" }, {"8-999-99-9999"} };
+            Console.WriteLine("Телефонный справочник:");
+            for (int i = 0; i < array2.GetLength(0); i++)
+            {
+                Console.Write($"{array2[i, 0]} {array2[i, 1]}");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
