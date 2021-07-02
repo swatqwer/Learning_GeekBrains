06f23f77be48a5839ba64babd42b2840f851268f
Anatoliy <swatqwer@gmail.com>
2021-07-02 18:27:27 +0700

    Урок 4, задание 1

---
+++ Less4_Ex1/Less4_Ex1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Less4_Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FullName("Иванов", "Иван", "Иванович"));
+            Console.WriteLine(FullName("Cерго", "Сергей", "Сергеевич"));
+            Console.WriteLine(FullName("Васечкин", "Весилий", "Васильевич"));
+            Console.WriteLine(FullName("Петро", "Петр", "Петрович"));
+            Console.ReadKey();
+        }
+        static string FullName(string lastName, string firstName, string patronymic)
+        {
+            return $"{firstName} {patronymic} {lastName}";
+        }
+    }
+}
