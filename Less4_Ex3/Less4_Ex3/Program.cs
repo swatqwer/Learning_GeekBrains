a968ad3780b2134516388399fbeaa6ccc4be2dc4
Anatoliy <swatqwer@gmail.com>
2021-07-02 19:13:16 +0700

    Урок 4, задание 3

---
+++ Less4_Ex3/Less4_Ex3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Less4_Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите порядковый номер месяца (1-12): ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num >= 1 && num <= 12)
+            {
+                Console.Write($"Время года: {GetSeason(num)}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: введите число от 1 до 12");
+            }
+            Console.ReadKey();
+        }
+        static string GetSeason(int numMonth)
+        {
+            string season = "";
+            switch (numMonth)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    season = ((Season)1).ToString();
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    season = ((Season)2).ToString();
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    season = ((Season)3).ToString();
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    season = ((Season)4).ToString();
+                    break;
+            }
+            return season.ToString();
+        }
+    }
+}
