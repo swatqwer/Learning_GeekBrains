d858e8b0b2cbb9b21bc5f226d0894a46f051045c
Anatoliy <swatqwer@gmail.com>
2021-07-02 18:41:51 +0700

    Урок 4, задание 2

---
+++ Less4_Ex2/Less4_Ex2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Less4_Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите числа через пробел: ");
+            string num = Console.ReadLine();
+            Console.Write($"Сумма введенных чисел = { Sum(num)}");
+            Console.ReadKey();
+        }
+        static int Sum(string num)
+        {
+            int i = 0;
+            string[] Number = num.Split(' ');
+
+            foreach (string s in Number)
+            {
+                i += Convert.ToInt32(s);
+            }
+            return i;
+        }
+    }
+}
