019ea7100148da0a3255ff44307ab7aefecb86a1
Anatoliy <swatqwer@gmail.com>
2021-07-06 17:53:40 +0700

    Урок 5, задание 2

---
+++ Less5_Ex2/Less5_Ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace Less5_Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.AppendAllText("startup.txt", $"{DateTime.Now:T}\n");
+            Console.ReadKey();
+        }
+    }
+}
