483b1e4eedc2f56f4cc0f5e1a04665eeff522560
Anatoliy <swatqwer@gmail.com>
2021-07-06 17:49:43 +0700

    Урок 5, задание 1

---
+++ Less5_Ex1/Less5_Ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Less5_Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите название текстового файла без расширения: ");
+            string filename = Console.ReadLine();
+            Console.Write("Введите произвольный набор данных: ");
+            string str = Console.ReadLine();
+            File.WriteAllText(filename+".txt", str);
+            Console.ReadKey();
+        }        
+    }
+}
