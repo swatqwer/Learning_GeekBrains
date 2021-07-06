b25d71d6d77b43bd45c366b8bca3b7193f6cef97
Anatoliy <swatqwer@gmail.com>
2021-07-06 18:09:22 +0700

    Урок 5, задание 3

---
+++ Less5_Ex3/Less5_Ex3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Less5_Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "Numbers.bin";
+            byte[] bForFile = new byte[5];
+
+            Console.WriteLine("Введите с клавиатуры произвольный набор чисел(0...255).");
+            var str = Console.ReadLine();
+            for (int i = 0; i < bForFile.Length; i++)
+            {
+                bForFile[i] = Convert.ToByte(str);
+            }
+            File.WriteAllBytes(filename, bForFile);
+            Console.ReadKey();
+        }
+    }
+}
